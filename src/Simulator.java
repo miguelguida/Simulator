6595a0f71fe9471cee95e8127878e022ba2816de
miguel <mglguida1@gmail.com>
2017-04-04 11:45:20 -0300

    Method disconnect changed to void type
    arrays for handles created

--- src/Simulator.java
+++ src/Simulator.java
@@ -7,29 +7,59 @@ import coppelia.remoteApi;
 public class Simulator {
 
     //create object handles, they are arrays that carry the handle.
-    private ArrayList<int> SonarHandle = new ArrayList<int>();
-    private ArrayList<int> LaserHandle = new ArrayList<int>();
-    private ArrayList<int> CameraHandle = new ArrayList<int>();
+    private ArrayList<int> sonarHandle = new ArrayList<int>();
+    private ArrayList<int> laserHandle = new ArrayList<int>();
+    private ArrayList<int> cameraHandle = new ArrayList<int>();
+    private ArrayList<int> handles = new ArrayList<int>();
     private String ip;
     private int port;
     private int clientID;
+    private boolean connectionStatus;
+    private int ret;
 
+    //DUVIDA: eu coloco um objeto remoteApi como parametro no construtor?
     public Simulator(String ip, int port){
     //here i can call connect() to start the conection to the simulator
         this.ip = ip;
         this.port = port;
+        connectionStatus = connect(ip, port);
     }
 
     //in this method, I'm going to set connection to the simulator,
     //get objects handles,
     //
-    public boolean connect(){
+    public boolean connect(String ip, int port){
 
-        clientID = simxStart("127.0.0.1", 25000, true, true, 5000, 50);
+        clientID = simxStart(ip, port, true, true, 5000, 50);
 
         if(clientID == -1){
             return false;
         }
+
+        ret = simxGetObjects(clientID, "sim_handle_all", handles, simx_opmode_oneshot_wait);
+        if (ret == remoteApi.simx_return_ok)
+            System.out.println("Got Handle");
+        else
+            System.out.format("Error: get vision handle returned with error");
+
+        ret = simxGetObjectHandle(clientID, "sonarHanle", sonarHandle, simx_opmode_oneshot_wait);
+        if (ret == remoteApi.simx_return_ok)
+            System.out.println("Got Handle");
+        else
+            System.out.format("Error: get vision handle returned with error");
+
+        ret = simxGetObjectHandle(clientID, "cameraHanle", cameraHandle, simx_opmode_oneshot_wait);
+        if (ret == remoteApi.simx_return_ok)
+            System.out.println("Got Handle");
+        else
+            System.out.format("Error: get vision handle returned with error");
+
+        ret = simxGetObjectHandle(clientID, "laserHanle", laserHandle, simx_opmode_oneshot_wait);
+        if (ret == remoteApi.simx_return_ok)
+            System.out.println("Got Handle");
+        else
+            System.out.format("Error: get vision handle returned with error");
+
         return true;
     }
 
@@ -45,9 +75,39 @@ public class Simulator {
         }
         return true;
     }
-    //updateSensors
+
+    public void updateSensors(){
+        //nao sei qual funcao que chama aq
+    }
     //readSensor --> implement a different method to each sensor (readSonar, readLaser).
-    //readPositionAbsolut --> robot position
-    //readPositionRelativ --> in relation to the robot
+    public ArrayList<int> readSonar(ArrayList<int> sonarHandle){
+        return sonarHandle.getValue();
+    }
+
+    public ArrayList<int> readLaser (ArrayList<int> laserHandle){
+        return laserHandle.getValue();
+    }
+
+    public ArrayList<int> readCamera(ArrayList<int> cameraHandle){
+        return cameraHandle.getValue();
+    }
+
+    //robot position
+    public float readPositionAbsolut(int clientID, int robotHandle){
+        float position;
+
+        simxGetObjectPosition(clientID, robotHandle, -1, position, simx_opmode_streaming );
+
+        return position;
+    }
+
+    //in relation to the robot
+    public float readPositionRelativ(int clientID, int objectHandle){
+        float position;
+
+        simxGetObjectPosition(clientID, objectHandle, -1, position, simx_opmode_buffer );
+
+        return position;
+    }
 
 }
